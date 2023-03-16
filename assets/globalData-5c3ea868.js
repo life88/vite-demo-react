cc002349fd4ae47d20ed6e4005a563950ce8f6c7
life88 <709886167@qq.com>
2023-03-16 10:24:50 +0000

    Deploying to gh-pages from @ life88/vite-demo-react@18c43371beafd2025de99de57e2b594e135c370c 🚀

---
+++ assets/globalData-5c3ea868.js
@@ -0,0 +1,1 @@
+const a={baseUrl:"https://jsonplaceholder.typicode.com",navigate:void 0},o=a;export{o as g};
---
+++ assets/index-57882d64.js
@@ -0,0 +1,1 @@
+import{n as t}from"./index-3a9f0ebf.js";function o(){return t.jsx("div",{children:"about page"})}export{o as default};

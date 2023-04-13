6758444f88794fe3b997a2de4edcd67a296d6be0
life88 <709886167@qq.com>
2023-04-13 14:26:09 +0000

    Deploying to gh-pages from @ life88/vite-demo-react@3b2f3b988c285d512181352e9500f1a33c5d94a6 🚀

---
+++ assets/index-309a2e69.js
@@ -0,0 +1,1 @@
+import{n as t}from"./index-3041ac6f.js";function o(){return t.jsx("div",{children:"about page"})}export{o as default};
--- assets/index-31b0ccb6.js
+++
@@ -1,1 +0,0 @@
-import{n as t}from"./index-fef9c5e0.js";function o(){return t.jsx("div",{children:"about page"})}export{o as default};
